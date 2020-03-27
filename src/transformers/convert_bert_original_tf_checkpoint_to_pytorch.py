c3738ef06bff09fedb2c96025c410ac24269246f
Bithiah Yuan <bithiahy@gmail.com>
2020-03-27 17:44:50 +0100

    changed convert tf to torch file

--- src/transformers/convert_bert_original_tf_checkpoint_to_pytorch.py
+++ src/transformers/convert_bert_original_tf_checkpoint_to_pytorch.py
@@ -20,7 +20,7 @@ import logging
 
 import torch
 
-from transformers import BertConfig, BertForPreTraining, load_tf_weights_in_bert
+from transformers import BertConfig, BertForPreTraining, load_tf_weights_in_bert, BertForSequenceClassification
 
 
 logging.basicConfig(level=logging.INFO)
